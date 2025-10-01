30bd7df45ef40fc135d200a134d1ecd16fb16733
Gus <guspearson78@gmail.com>
2025-10-01 08:20:33 -0700

    added new script
    for peices

---
+++ OtherSpaceStationPeices.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class OtherSpaceStationPeices : MonoBehaviour
+{
+    public int X;
+    public int Y;
+    public int R; //rotation
+    public Sprite me;
+
+    void Start() {
+        //do peice selection
+    }
+    
+}
