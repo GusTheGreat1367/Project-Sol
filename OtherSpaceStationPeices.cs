d08e884348de7203b505a7b372e0c10224c20ca4
Gus <guspearson78@gmail.com>
2025-10-01 08:48:40 -0700

    Update OtherSpaceStationPeices.cs

--- OtherSpaceStationPeices.cs
+++ OtherSpaceStationPeices.cs
@@ -2,13 +2,30 @@ using UnityEngine;
 
 public class OtherSpaceStationPeices : MonoBehaviour
 {
-    public int X;
-    public int Y;
-    public int R; //rotation
-    public Sprite me;
-
-    void Start() {
-        //do peice selection
+    class Peices//each peice will be a struct in this class
+    {
+        struct Structual_peice
+        { //make a struct like this for each peice
+            public Script StructualPeice;
+            public int X;
+            public int Y;
+            public int R; //rotation
+        }
+        struct ResourceMiner_peice
+        {
+            public Script ResourceMinerPeice;
+            public int X;
+            public int Y;
+            public int R; //rotation
+        }
+    }
+    public void PeiceSelection(Peices peice){
+        
+        void OnMouseDown()
+        {
+            //make it select a peice and clone it and communicate to RootSpaceStationPeice
+            //make it do it once a time
+        }
     }
     
 }
