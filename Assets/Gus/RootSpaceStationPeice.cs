3475b211d72883708fc380f56511e393cf2f2670
Gus <guspearson78@gmail.com>
2026-01-13 18:25:11 -0800

    Update RootSpaceStationPeice.cs

--- Assets/Gus/RootSpaceStationPeice.cs
+++ Assets/Gus/RootSpaceStationPeice.cs
@@ -1,54 +1,117 @@
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using OtherSpaceStationPeices;
 
-public class Root // might not matter bc Root
-// is an unmovable object with the sole purpose of linking together a space station, not moving!
+namespace RootSpaceStationPeice
 {
-    public int X;
-    public int Y;
-    public int R;
-}
-
-
-public class RootSpaceStationPeice : MonoBehaviour
-{
-
-    public OtherSpaceStationPeices referance;
-    
-    var pieces = 0;
-    void Start()
+    public class Root //the only reason we'd need this is to link the pieces together
     {
-        X = 0;
-        Y = 0;
-        Z = 0;
-    }
-    void Disp(msg, txtsize, x, y)//display a message the the screen
-    {
-        // do the UI rendering "fun" where the msg fades after 2.5 seconds
+        public int X;
+        public int Y;
+        public int R;
     }
 
-    void Update()
+
+    public class RootSpaceStationPeice : MonoBehaviour
     {
-        if (X != 0 || Y != 0 || R != 0)
+        private int pieces = 0;
+        
+        // Added required fields that were missing
+        private int X;
+        private int Y;
+        private int Z;
+        private int R;
+
+        private class FadingMessage
+        {
+            public string text;
+            public int size;
+            public Rect rect;
+            public float timer;
+        }
+
+        private List<FadingMessage> activeMessages = new List<FadingMessage>();
+
+        void Start()
         {
             X = 0;
             Y = 0;
             Z = 0;
+            R = 0;
         }
-        if (OtherSpaceStationPeices.SelectedPiece /*Is touching root*/)
+
+        void Disp(string msg, int txtsize, int x, int y)//display a message the the screen
         {
-            pieces += 1;
-            //detect where the piece is touching and append it to that area.
+            
+            // Prevent duplicate message spamming
+            if (activeMessages.Count > 0 && activeMessages[activeMessages.Count - 1].text == msg)
+            {
+                return;
+            }
+
+            activeMessages.Add(new FadingMessage
+            {
+                text = msg,
+                size = txtsize,
+                rect = new Rect(x + 10, y + 10, 500, 100), // Offset slightly and give width
+                timer = 2.5f
+            });
         }
-        if (pieces == 0)
+
+        void OnGUI()
         {
-            Disp("Attach a piece to the root piece to get started!", 15, 0, 5);
+            for (int i = activeMessages.Count - 1; i >= 0; i--)
+            {
+                var msg = activeMessages[i];
+                msg.timer -= Time.deltaTime;
+
+                if (msg.timer <= 0)
+                {
+                    activeMessages.RemoveAt(i);
+                    continue;
+                }
+
+                GUIStyle style = new GUIStyle();
+                style.fontSize = msg.size;
+                // Fade out in the last 1 second
+                float alpha = Mathf.Clamp01(msg.timer);
+                style.normal.textColor = new Color(1, 1, 1, alpha);
+                
+                GUI.Label(msg.rect, msg.text, style);
+            }
         }
-        if (pieces == 64) // to prevent crashing the game and messing up storage later update to a better #
+
+        void Update()
         {
-            Disp("Error, you cannot have more than 64 pieces on the space station at a time", 20, 0, 0);
-            //Don't allow anymore pieces to be added
+            if (X != 0 || Y != 0 || R != 0)
+            {
+                X = 0;
+                Y = 0;
+                Z = 0;
+            }
+            if(Input.GetKey(KeyCode.P))
+            {
+                //snap the piece
+                pieces += 1;
+            }
+            
+            // Commenting out this check as OtherSpaceStationPeices.SelectedPiece is not accessible staticly
+            // if (OtherSpaceStationPeices.OtherSpaceStationPeices.SelectedPiece /*Is touching root*/)
+            // {
+            //    pieces += 1;
+                //detect where the piece is touching and append it to that area.
+            // }
+
+            if (pieces == 0)
+            {
+                Disp("Attach a piece to the root piece to get started!", 15, 0, 5);
+            }
+            if (pieces == 64) // to prevent crashing the game and messing up storage later update to a better #
+            {
+                Disp("Error, you cannot have more than 64 pieces on the space station at a time", 20, 0, 0);
+                enabled = false; // Stop the script instead of 'break'
+            }
         }
     }
 }
