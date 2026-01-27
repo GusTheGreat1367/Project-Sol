c63bfea599010fcb091773e7243cb8430276799f
Gus <guspearson78@gmail.com>
2026-01-27 12:17:12 -0800

    Implement delay for resource generation in Activation
    Added a delay in the Activation method to control resource generation.

--- Assets/Gus/ResourceMiner.cs
+++ Assets/Gus/ResourceMiner.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 using System.Collections.Generic;
 using SpaceStation;
 using OtherSpaceStationPeices;
+using System;
+using System.Threading;
 
 namespace ResourceMiner_piece //This entire namespace works for all of the pieces, just change the Activation()
 {
     public class ResourceMiner_piece : MonoBehaviour
     {
+        public string type = "functional";
         int X = 0;
         int Y = 0;
         int R = 0;
@@ -29,7 +32,12 @@ namespace ResourceMiner_piece //This entire namespace works for all of the piece
         void Activation()//on build.cs or RootSpaceStation.cs call Structual_piece.Activation(); to start the function
         {
             //control for the piece
-            
+            Thread.Sleep(2000); // every 2 seconds, add 1 to resources and 1 to credits
+            SpaceStation.resources["Ore"] += 10;
+            SpaceStation.resources["O2"] += 10;
+            SpaceStation.resources["Carbon"] += 100;
+            SpaceStation.resources["H2O"] += 100;
+            SpaceStation.credits += 25;
         }
     }
 }
