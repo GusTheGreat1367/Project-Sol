57e9f067772d7336ad5af33ee1e7f8490db7c5c6
Gus <guspearson78@gmail.com>
2026-02-01 21:29:55 -0800

    Add level-based timing and upgrade functionality
    This script might be done!

--- Assets/Gus/ResourceMiner.cs
+++ Assets/Gus/ResourceMiner.cs
@@ -9,6 +9,19 @@ namespace ResourceMiner_piece //This entire namespace works for all of the piece
 {
     public class ResourceMiner_piece : MonoBehaviour
     {
+        public int level = 1;
+        public int UpCost = 2500;
+        public var time = new Dictionary<int, int> ();
+        time.Add(1, 5000); // you could easily automate this just by using a for loop
+        time.Add(2, 4500);
+        time.Add(3, 4000);
+        time.Add(4, 3500);
+        time.Add(5, 3000);
+        time.Add(6, 2500);
+        time.Add(7, 2000);
+        // ect. Decreasing by 500 each time until it reaches 2000 (max level)
+
+
         public string type = "functional";
         int X = 0;
         int Y = 0;
@@ -32,12 +45,24 @@ namespace ResourceMiner_piece //This entire namespace works for all of the piece
         void Activation()//on build.cs or RootSpaceStation.cs call Structual_piece.Activation(); to start the function
         {
             //control for the piece
-            Thread.Sleep(2000); // every 2 seconds, add 1 to resources and 1 to credits
+            Thread.Sleep(time[level]); 
             SpaceStation.resources["Ore"] += 10;
             SpaceStation.resources["O2"] += 10;
             SpaceStation.resources["Carbon"] += 100;
             SpaceStation.resources["H2O"] += 100;
             SpaceStation.credits += 25;
         }
+        void upgrade() // in space station.cs 
+        {
+            if (time[level] < 7)
+            {
+                if (SpaceStation.credits >= UpCost)
+                {
+                    SpaceStation.credits -= UpCost;
+                    level += 1;
+                    UpCost += 2500;
+                }
+            }
+        }        
     }
 }
